42c0c5406768cc073b7ba7f98e4a4efac66ea9e7
Ayesha-098 <ayeshamasood945@gmail.com>
2026-07-25 02:26:49 +0500

    Initial commit

--- studentportal/settings.py
+++ studentportal/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # --- Security (fine for local/demo use, change SECRET_KEY before deploying) ---
 SECRET_KEY = "django-insecure-demo-key-change-me"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["educational-portal-production.up.railway.app",]
 
 # --- Apps ---
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -69,7 +70,11 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
